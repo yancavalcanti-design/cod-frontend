1eb04553cfd11bc992096c5338ac430cc7e217f8
YAN LEAO CAVALCANTI <yan_cavalcanti@estudante.sesisenai.org.br>
2026-09-09 17:09:22 -0300

    primeiro projeto

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div classname="cont-app">
+      <h1>trampando com react</h1>
+      <h2>Onde posso trampar como dev?</h2>
+      <p>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</p>
+      <p>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</p>
+      <p>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.</p>
+      <p>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</p>
+
+      <h2>Trampos com tecnologia mas sem programação</h2>
+      <p>Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.</p>
+
+      <ul>
+        <li>Product Management & Negócios</li>
+        <li>Design e Experiência do Usuário (UI/UX)</li>
+        <li>Gestão de Projetos e Agilidade</li>
+        <li>Dados e Inteligência de Negócio</li>
+        <li>Vendas Técnicas e Sucesso do Cliente</li>
+      </ul>
+
+      
+
+    </div>
+  )
+}
+
+export default App
